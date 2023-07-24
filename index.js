3879fac83a28624aebb9db64d0cd9351975f961a
piomin <piotr.minkowski@gmail.com>
2023-07-24 17:08:06 +0200

    add all HTTP methods

--- index.js
+++ index.js
@@ -8,29 +8,58 @@ const apiUrl = process.env.REACT_APP_BACKEND_URL
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'client/build')));
+app.use(express.json());
 
 app.get("/persons", (req, res) => {
   axios.get(apiUrl + "/persons")
   .then(function (response) {
-    // handle success
-    console.log(response.data);
     res.json(response.data);
   })
   .catch(function (error) {
-    // handle error
     console.log(error);
   })
   .finally(function () {
-    // always executed
   });
-//   console.log(response.data)
-//   res.json(response.data);
-//   res.json([{id: 1, name: "Piotr", gender: "MALE", age: 20 }]);
+});
+
+app.get("/persons/:id", (req, res) => {
+  axios.get(apiUrl + "/persons/" + req.params.id)
+  .then(function (response) {
+    res.json(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+  });
+});
+
+app.post("/persons", (req, res) => {
+  console.log("New person:" + req.body);
+  axios.post(apiUrl + "/persons", {
+    name: req.body.name,
+    gender: req.body.gender,
+    age: req.body.age,
+    address: {
+      city: "Warsaw",
+      street: "None",
+    }
+  }, {
+    headers: {'Content-Type': 'application/json'}
+  })
+  .then(function (response) {
+    res.json(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+  });
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-  });
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+});
   
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
